c4a409f0b46dec64a6f202c64eca08e06cbb2761
axelldev <axellsolisrubio@gmail.com>
2024-09-16 18:04:20 -0600

    fix: force tasks page to be dynamic

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,6 +1,7 @@
 import TaskForm from '@/components/TaskForm'
 import TasksList from '@/components/TasksList'
 
+export const dynamic = 'force-dynamic'
 export default async function TasksPage() {
   return (
     <div className="max-w-lg">
